d99d683963eb256024ccded6b3eb46e86b087c18
Ash Powell <ash-powell@hotmail.co.uk>
2019-01-15 19:38:32 +0000

    Apply fixes from StyleCI

--- src/Listeners/AnalyticTrackedListener.php
+++ src/Listeners/AnalyticTrackedListener.php
@@ -3,18 +3,6 @@
 namespace AshPowell\APAnalytics\Listeners;
 
 use AshPowell\APAnalytics\Events\AnalyticTracked;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
-
-use Illuminate\Bus\Queueable;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
-use Log;
 
 class AnalyticTrackedListener
 {
