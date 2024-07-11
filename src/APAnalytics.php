935ebaa5df6cf6833fe1d5c0cb07232bfb4c750f
Ash Powell <ash-powell@hotmail.co.uk>
2024-07-11 11:32:02 +0100

    Update APAnalytics.php

--- src/APAnalytics.php
+++ src/APAnalytics.php
@@ -34,16 +34,44 @@ class APAnalytics
      */
     public function track($collection, $items, $userId = null, $params = [])
     {
-        Track::dispatch($collection, $items, $userId, $params);
+        try {
+            Track::dispatch($collection, $items, $userId, $params);
 
-        return true;
+            return true;
+        } catch (\Exception $e) {
+            report($e);
+
+            return null;
+        } catch (\Throwable $e) {
+            report($e);
+
+            return null;
+        } catch (\MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
+            report($e);
+
+            return null;
+        }
     }
 
     public function update($collection, $item, $params)
     {
-        Track::dispatch($collection, $item, null, $params, 'update');
+        try {
+            Track::dispatch($collection, $item, null, $params, 'update');
 
-        return true;
+            return true;
+        } catch (\Exception $e) {
+            report($e);
+
+            return null;
+        } catch (\Throwable $e) {
+            report($e);
+
+            return null;
+        } catch (\MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
+            report($e);
+
+            return null;
+        }
     }
 
     /**
@@ -58,137 +86,151 @@ class APAnalytics
      */
     public function show($collection, $interval = 'count', $timeframe = null, $filters = null, $groupBy = null, $distinct = null)
     {
-        $start          = $timeframe ? Arr::get($timeframe, 'start') : null;
-        $end            = $timeframe ? Arr::get($timeframe, 'end') : null;
-        $matchArray     = [];
-        $filters        = valid_json($filters) ? (array) json_decode($filters) : $filters;
-        $intervalFormat = '%Y-%m-%dT%H';
-        $aggregate      = [];
-        $model          = $this->namespace.Str::studly(Str::singular($collection)).'Analytic';
+        try {
+            $start          = $timeframe ? Arr::get($timeframe, 'start') : null;
+            $end            = $timeframe ? Arr::get($timeframe, 'end') : null;
+            $matchArray     = [];
+            $filters        = valid_json($filters) ? (array) json_decode($filters) : $filters;
+            $intervalFormat = '%Y-%m-%dT%H';
+            $aggregate      = [];
+            $model          = $this->namespace.Str::studly(Str::singular($collection)).'Analytic';
 
-        if (! class_exists($model)) {
-            throw new InvalidArgumentException("Model {$model} does not exist.");
-        }
+            if (! class_exists($model)) {
+                throw new InvalidArgumentException("Model {$model} does not exist.");
+            }
 
-        if ($filters) {
-            if (is_array($filters)) {
-                $filters = Arr::first($filters);
+            if ($filters) {
+                if (is_array($filters)) {
+                    $filters = Arr::first($filters);
 
-                if (is_array($filters) && count($filters) > 1) {
-                    foreach ($filters as $filter) {
-                        $matchArray = $this->matchPropertyNameToValue($matchArray, $filter);
+                    if (is_array($filters) && count($filters) > 1) {
+                        foreach ($filters as $filter) {
+                            $matchArray = $this->matchPropertyNameToValue($matchArray, $filter);
+                        }
+                    } else {
+                        $matchArray = $this->matchPropertyNameToValue($matchArray, $filters);
                     }
-                } else {
-                    $matchArray = $this->matchPropertyNameToValue($matchArray, $filters);
                 }
             }
-        }
 
-        if (! app()->runningInConsole()) {
-            abort_unless(auth()->check() && auth()->user()->can('view', [new $model, $matchArray]), 403, 'You dont have permission to view these analytics');
-        }
+            if (! app()->runningInConsole()) {
+                abort_unless(auth()->check() && auth()->user()->can('view', [new $model, $matchArray]), 403, 'You dont have permission to view these analytics');
+            }
 
-        if ($start) {
-            $matchArray['created_at']['$gte'] = mongoTime($start);
-        }
+            if ($start) {
+                $matchArray['created_at']['$gte'] = mongoTime($start);
+            }
 
-        if ($end) {
-            $matchArray['created_at']['$lt'] = mongoTime($end);
-        }
+            if ($end) {
+                $matchArray['created_at']['$lt'] = mongoTime($end);
+            }
 
-        if ($matchArray) {
-            $aggregate[] = ['$match' => $matchArray];
-        }
+            if ($matchArray) {
+                $aggregate[] = ['$match' => $matchArray];
+            }
 
-        if ($distinct) {
-            $aggregate[] =  [
-                '$group' => [
-                    '_id'        => '$'.$distinct,
-                    'created_at' => [
-                        '$last' => '$created_at',
+            if ($distinct) {
+                $aggregate[] =  [
+                    '$group' => [
+                        '_id'        => '$'.$distinct,
+                        'created_at' => [
+                            '$last' => '$created_at',
+                        ],
                     ],
-                ],
-            ];
+                ];
+            }
+
+            if ($interval != 'count') {
+                if ($interval == 'daily') {
+                    $intervalFormat = '%Y-%m-%d';
+                }
+
+                if ($interval == 'weekly') {
+                    $intervalFormat = '%Y-%U';
+                }
+
+                if ($interval == 'monthly' || $interval == 'growth') {
+                    $intervalFormat = '%Y-%m';
+                }
+
+                $aggregate[] =  [
+                    '$group' => [
+                        '_id' => [
+                            '$dateToString' => ['date' => '$created_at', 'format' => $intervalFormat],
+                        ],
+                        'count' => [
+                            '$sum' => 1,
+                        ],
+                        'created_at' => [
+                            '$last' => '$created_at',
+                        ],
+                    ],
+                ];
+
+                $aggregate[] = ['$sort' => ['created_at' => 1]];
+
+                $aggregate[] = [
+                    '$project' => [
+                        '_id'        => 0,
+                        'created_at' => 1,
+                        'count'      => 1,
+                    ],
+                ];
+            }
+
+            if ($interval == 'count' && $groupBy != null) {
+                $nested      = Str::contains($groupBy, '.');
+                $group       = $nested ? Str::before($groupBy, '.') : $groupBy;
+                $nestedGroup = $nested ? Str::after($groupBy, '.') : $groupBy;
+
+                if ($nested) {
+                    $aggregate[] = ['$unwind' => '$'.$group];
+                }
+
+                $aggregate[] =  [
+                    '$group' => [
+                        '_id'   => '$'.$groupBy,
+                        'count' => [
+                            '$sum' => 1,
+                        ],
+                    ],
+                ];
+
+                $aggregate[] = ['$sort' => ['count' => 1]];
+
+                $aggregate[] = [
+                    '$project' => [
+                        '_id'        => 0,
+                        $nestedGroup => '$_id',
+                        'count'      => 1,
+                    ],
+                ];
+            }
+
+            $data = $model::raw(function ($collection) use ($matchArray, $interval, $aggregate, $groupBy) {
+                if ($interval == 'count' && ! $groupBy) {
+                    return $collection->count($matchArray);
+                }
+
+                if ($aggregate) {
+                    return $collection->aggregate($aggregate, ['allowDiskUse' => true]);
+                }
+            });
+
+            return $data;
+        } catch (\Exception $e) {
+            report($e);
+
+            return null;
+        } catch (\Throwable $e) {
+            report($e);
+
+            return null;
+        } catch (\MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
+            report($e);
+
+            return null;
         }
-
-        if ($interval != 'count') {
-            if ($interval == 'daily') {
-                $intervalFormat = '%Y-%m-%d';
-            }
-
-            if ($interval == 'weekly') {
-                $intervalFormat = '%Y-%U';
-            }
-
-            if ($interval == 'monthly' || $interval == 'growth') {
-                $intervalFormat = '%Y-%m';
-            }
-
-            $aggregate[] =  [
-                '$group' => [
-                    '_id' => [
-                        '$dateToString' => ['date' => '$created_at', 'format' => $intervalFormat],
-                    ],
-                    'count' => [
-                        '$sum' => 1,
-                    ],
-                    'created_at' => [
-                        '$last' => '$created_at',
-                    ],
-                ],
-            ];
-
-            $aggregate[] = ['$sort' => ['created_at' => 1]];
-
-            $aggregate[] = [
-                '$project' => [
-                    '_id'        => 0,
-                    'created_at' => 1,
-                    'count'      => 1,
-                ],
-            ];
-        }
-
-        if ($interval == 'count' && $groupBy != null) {
-            $nested      = Str::contains($groupBy, '.');
-            $group       = $nested ? Str::before($groupBy, '.') : $groupBy;
-            $nestedGroup = $nested ? Str::after($groupBy, '.') : $groupBy;
-
-            if ($nested) {
-                $aggregate[] = ['$unwind' => '$'.$group];
-            }
-
-            $aggregate[] =  [
-                '$group' => [
-                    '_id'   => '$'.$groupBy,
-                    'count' => [
-                        '$sum' => 1,
-                    ],
-                ],
-            ];
-
-            $aggregate[] = ['$sort' => ['count' => 1]];
-
-            $aggregate[] = [
-                '$project' => [
-                    '_id'        => 0,
-                    $nestedGroup => '$_id',
-                    'count'      => 1,
-                ],
-            ];
-        }
-
-        $data = $model::raw(function ($collection) use ($matchArray, $interval, $aggregate, $groupBy) {
-            if ($interval == 'count' && ! $groupBy) {
-                return $collection->count($matchArray);
-            }
-
-            if ($aggregate) {
-                return $collection->aggregate($aggregate, ['allowDiskUse' => true]);
-            }
-        });
-
-        return $data;
     }
 
     private function matchPropertyNameToValue($matchArray, $filter)
